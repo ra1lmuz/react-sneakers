74f8241372120221044e7934798405c20b2be3d5
Alan Bogov avavion <avavionmvm@gmail.com>
2023-02-04 09:37:22 +0300

    Add slider

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,35 @@
 import { NavLink } from "react-router-dom";
 import { CartIcon, HeartIcon, OrderIcon } from "../Icons/Icons";
-
-import logo from '../../assets/logo.png';
+import Logo from "./Logo/Logo";
 
 const CartButton = () => {
-    return (
-        <button>
-            <CartIcon /> <span>1205 руб.</span>
-        </button>
-    );
+  return (
+    <button>
+      <CartIcon /> <span>1205 руб.</span>
+    </button>
+  );
 };
 
 const Header = () => {
-    return (
-        <header className="header">
-            <div className="logo">
-                <img src={logo} alt="React Sneakers" width={40} height={40} />
+  return (
+    <header className="header">
+      <NavLink to="/">
+        <Logo />
+      </NavLink>
 
-                <div>
-                    <h1>React Sneakers</h1>
-                    <p>Магазин лучших кроссовок</p>
-                </div>
-            </div>
+      <nav>
+        <CartButton />
 
-            <nav>
-                <CartButton />
+        <NavLink to="/favorites">
+          <HeartIcon />
+        </NavLink>
 
-                <NavLink to="/favorites">
-                    <HeartIcon />
-                </NavLink>
-
-                <NavLink to="/orders">
-                    <OrderIcon />
-                </NavLink>
-            </nav>
-        </header>
-    )
-}
+        <NavLink to="/orders">
+          <OrderIcon />
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,19 +3,30 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../pages/Root";
 
 // React Lazy Imports
-const HomePage = React.lazy(() => import('../pages/HomePage'));
+const HomePage = React.lazy(() => import("../pages/HomePage"));
+const OrdersPage = React.lazy(() => import("../pages/OrdersPage"));
+const FavoritesPage = React.lazy(() => import("../pages/FavoritesPage"));
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
         path: "/",
-        element: <Root />,
-        children: [
-            {
-                path: "/",
-                element: <HomePage />
-            }
-        ]
-    }
+        element: <HomePage />,
+        index: true
+      },
+      {
+        path: "/orders",
+        element: <OrdersPage />,
+      },
+      {
+        path: "/favorites",
+        element: <FavoritesPage />,
+      },
+    ],
+  },
 ]);
 
 export default router;
